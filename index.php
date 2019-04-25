635fcdee7c9a56d4fec023804f519e44c6f1b6fb
nomad <nomad.93@hotmail.com>
2019-04-25 10:52:05 +0200

    Update index.php
    Find the lowest missing positive number from array

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
     
 <?php
+
 $array = array();
+
 //generate 5 unique numbers
 while(count($array) < 5) {
     $rand = rand(-15, 15);
@@ -9,4 +11,28 @@ while(count($array) < 5) {
     }
 }
 
+$lowestMissingNumber = NULL;
+
 var_dump($array);
+
+//Should be remove negative numbers from array to reduce runtime and resources.
+for ($i = max($array); $i > min($array)-1; $i--) { 
+
+    $inarray = in_array($i, $array);
+    $compare = $i > $lowestMissingNumber && $inarray;
+
+    //Of course if we remove the negative numbers from array then the first condition not necessary.
+    if ($i <= 0 || $compare) {
+        continue;
+    } else {
+        if(!in_array($i, $array)){
+            $lowestMissingNumber = $i;
+        }
+    }
+}
+
+if(isset($lowestMissingNumber)){
+    echo "Lowest missing number: $lowestMissingNumber";
+} else{
+    echo "No missing positive number in array!";
+}
